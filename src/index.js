61a7ddd9f859f8d6e0568695aeeb6cf8f8fde4f4
sophiacofone <sophiacofone@gmail.com>
2023-04-23 15:44:58 -0400

    n

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootswatch/dist/quartz/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
