fd1d9ab3688832acd3db4da90df84bc70c481c86
sophiacofone <sophiacofone@gmail.com>
2023-04-23 16:42:13 -0400

    Update login-screen.js

--- src/screens/login-screen.js
+++ src/screens/login-screen.js
@@ -1,60 +1,64 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router";
-import { loginThunk } from "../services/users/users-thunks";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router";
+import {loginThunk} from "../services/users/users-thunks";
 
 function LoginScreen() {
-  const { currentUser } = useSelector((state) => state.users);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const login = async () => {
-    try {
-      await dispatch(loginThunk({ username, password }));
-      navigate("/profile");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return (
-    <div>
-      <h1>Login</h1>
-      <div className="form-group">
-        <label>Username</label>
-        <input
-          type="text"
-          className="form-control"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </div>
-      <button onClick={login} className="btn btn-primary">
-        Login
-      </button>
-      <div>
-        {currentUser && (
-          <div>
-            <h2>{currentUser.username}</h2>
-            <h2>{currentUser.password}</h2>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    const {currentUser} = useSelector((state) => state.users);
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin= async (e) => {
+        e.preventDefault(); // prevent default form submission behavior
+        try {
+            await dispatch(loginThunk({ username, password }));
+            navigate("/profile");
+        } catch (e) {
+            alert(e);
+        }
+    };
+
+    return (
+        <div>
+            <div className="container mx-auto w-50 text-center mt-3">
+                <h1 className="h3 mb-3 font-weight-normal">Please Log in</h1>
+                <h2><i className="bi bi-music-note-beamed"></i></h2>
+                <form onSubmit={handleLogin}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            id="inputUser"
+                            className="form-control"
+                            placeholder="Username"
+                            required=""
+                            autoFocus="">
+                        </input>
+                        <label htmlFor="inputUser" className="sr-only"></label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            id="inputPassword"
+                            className="form-control"
+                            placeholder="Password"
+                            required="">
+                        </input>
+                        <label htmlFor="inputPassword" className="sr-only"></label>
+                    </div>
+                    <button className="btn btn-lg btn-primary w-100 mt-3" type="submit">Login</button>
+                </form>
+                <div>
+                    <a href="/register" className="btn btn-link">Need to register?</a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LoginScreen;
