9581d7b2242cbfbdb3c321c8e0047be78e4e8fc4
sophiacofone <sophiacofone@gmail.com>
2023-04-27 22:11:42 -0400

    homescreen

--- src/screens/home-screen.js
+++ src/screens/home-screen.js
@@ -1,14 +1,88 @@
 import React, {useState} from "react";
+import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router";
 import {Link} from "react-router-dom";
+import {
+    findAlbumImageId,
+    findAlbumNameId, findArtistGenreId,
+    findArtistImageId,
+    findArtistNameId,
+    findLikesByUserId, findTrackGenreId, findTrackImageId, findTrackNameId
+} from "../spotify/likes-service";
+import {findAllUsersThunk} from "../services/users/users-thunks";
+
+
 
 function HomeScreen() {
-    const {currentUser} = useSelector((state) => state.users);
+    const { currentUser, users } = useSelector((state) => state.users);
+    const [likes, setLikes] = useState([]);
+    const [likedGenres, setLikedGenres] = useState([]);
 
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const fetchLikes = async () => {
+        const likes = await findLikesByUserId(currentUser._id);
+        const likesData = await Promise.all(
+            likes.map(async (like) => {
+                let name;
+                let image;
+                let genre;
+                if (like.type === "album") {
+                    name = await findAlbumNameId(like.musicThingId)
+                    image = await findAlbumImageId(like.musicThingId);
+                } else if (like.type === "artist") {
+                    name = await findArtistNameId(like.musicThingId)
+                    image = await findArtistImageId(like.musicThingId);
+                    genre = await findArtistGenreId(like.musicThingId);
+                } else if (like.type === "track") {
+                    name = await findTrackNameId(like.musicThingId)
+                    image = await findTrackImageId(like.musicThingId);
+                    genre = await findTrackGenreId(like.musicThingId);
+                }
+                return {
+                    ...like,
+                    name,
+                    image,
+                    genre,
+                };
+            })
+        );
+        setLikes(likesData);
+
+        const genresSet = new Set();
+
+        likesData.forEach((like) => {
+            if (like.genre) {
+                like.genre.forEach((genre) => {
+                    genresSet.add(genre);
+                });
+            }
+        });
+
+        const uniqueGenres = Array.from(genresSet);
+        setLikedGenres(uniqueGenres);
+    };
+
+    useEffect(() => {
+        dispatch(findAllUsersThunk());
+    }, []);
+
+    useEffect(() => {
+        if (currentUser && currentUser._id) {
+            fetchLikes();
+        }
+    }, [currentUser]);
+
+    const randomIndex = Math.floor(Math.random() * likes.length);
+
+    const randomElement = likes[randomIndex];
+
+    const randomIndex1 = Math.floor(Math.random() * users.length);
+
+    const randomElement1 = users[randomIndex1];
 
     return (
         <div>
@@ -29,37 +103,122 @@ function HomeScreen() {
                             <div className="mt-2">
                                 <Link to="/login"  className='btn btn-primary'> Log in »</Link>
                             </div>
+                            <div className="row mt-2">
+                                <div className="col-4">
+                                </div>
+                                <div className="col-4 text-center">
+                                    <div>
+                                        <div className="card text-white mb-1">
+                                            <div className="card-body p-2">
+                                                <div className="row">
+                                                    <div className="d-flex justify-content-center align-items-center">
+                                                        {randomElement1 && (
+                                                            <p className="card-text"> <strong> {randomElement1.username} </strong> joined SoundSynergy!</p>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-4">
+                                </div>
+                            </div>
                         </h6>
                     </div>
+                    )}
+                    {currentUser && currentUser.role === "USER" && randomElement && (
+                        <div className="row">
+                        <div className="col-3">
+                        </div>
+                        <div className="col-6">
+                            <Link to={`/search/artist/${randomElement.musicThingId}`} style={{ textDecoration: 'none' }}>
+                                <div className="card text-white mb-1">
+                                    <div className="card-body p-2">
+                                        <div className="row">
+                                            <div className="col-10 d-flex justify-content-center align-items-center">
+                                                <p className="card-text">You liked <strong>{randomElement.name}</strong>!</p>
+                                            </div>
+                                            <div className="col-2">
+                                                <img
+                                                    src={randomElement.image}
+                                                    className="img-thumbnail"
+                                                    style={{ width: "4rem", height: "4rem" }}
+                                                    alt={randomElement.name}
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                       </div>
+                        <div className="col-3">
+                        </div>
+                        </div>
+                    )}
+                    {currentUser && currentUser.role === "BUSINESS" && (
+                        <div>
+
+                        </div>
                     )}
                 </div>
                 <div className="card-body">
                     <div className="container marketing">
                         <div className="row">
-                            <div className="col-lg-4 text-center">
-                                <h2 className="fw-normal">Explore</h2>
-                                <p>See stats on your listening history, most popular genres, and more.</p>
-                                {currentUser ? (
-                                    <Link to="/analysis" className="btn btn-secondary">
-                                        <span className="">Analyze »</span>
-                                    </Link>
-                                ) : (
-                                    <button className="btn btn-secondary">Please log in »</button>
+                            <div className="col-lg-6 text-center">
+                                {currentUser === null ? (
+                                    <div>
+
+                                        <h2 className="fw-normal">Share</h2>
+                                        <p>Search for any song, and see which of your friends are most likely to enjoy it.</p>
+                                        <Link to="/search"  className='btn btn-secondary'>
+                                            <span className=""> Search »</span>
+                                        </Link>
+                                    </div>
+                                ) : currentUser.role === "USER" ? (
+                                    <div>
+                                        <h2 className="fw-normal">Share</h2>
+                                        <p>Search for any song, and see which of your friends are most likely to enjoy it.</p>
+                                        <Link to="/search"  className='btn btn-secondary'>
+                                            <span className=""> Search »</span>
+                                        </Link>
+                                    </div>
+                                )   : (
+                                    <div>
+                                        <h2 className="fw-normal">Save</h2>
+                                        <p>Search for any song, and save it so your followers know what you play.</p>
+                                        <Link to="/search"  className='btn btn-secondary'>
+                                            <span className=""> Search »</span>
+                                        </Link>
+                                    </div>
                                 )}
                             </div>
-                            <div className="col-lg-4 text-center">
-                                <h2 className="fw-normal">Connect</h2>
-                                <p>Search for any song, and see which of your friends are most likely to enjoy it.</p>
-                                <Link to="/search"  className='btn btn-secondary'>
-                                    <span className=""> Search »</span>
-                                </Link>
-                            </div>
-                            <div className="col-lg-4 text-center">
-                                <h2 className="fw-normal">Discover</h2>
-                                <p>Learn about local restaurants, cafes, and shops that play music you will love. </p>
-                                <Link to="/analysis"  className='btn btn-secondary'>
-                                    <span className=""> Discover »</span>
-                                </Link>
+                            <div className="col-lg-6 text-center">
+                                {currentUser === null ? (
+                                    <div>
+                                        <h2 className="fw-normal">Connect</h2>
+                                        <p>Learn about local restaurants, cafes, and shops that play music you will love. </p>
+                                        <div className="mt-2 mb-2">
+                                            <Link to="/login"  className='btn btn-secondary'> Log in »</Link>
+                                        </div>
+                                    </div>
+                                    ) : currentUser.role === "USER" ? (
+                                    <div>
+                                        <h2 className="fw-normal">Connect</h2>
+                                        <p>Learn about local restaurants, cafes, and shops that play music you will love. </p>
+                                        <Link to="/connect"  className='btn btn-secondary'>
+                                            <span className=""> Connect »</span>
+                                        </Link>
+                                    </div>
+                                    )   : (
+                                    <div>
+                                    <h2 className="fw-normal">Analyze</h2>
+                                    <p>Learn about your follower's music preferences. </p>
+                                        <Link to="/analysis"  className='btn btn-secondary'>
+                                            <span className=""> Analysis »</span>
+                                        </Link>
+                                    </div>
+                                    )}
                             </div>
                         </div>
                     </div>
